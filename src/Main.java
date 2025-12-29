21478109655f201f69ef741c3634d381eab4c663
dandrusiv <dmytro.andrusiv@gmail.com>
2025-12-29 20:15:00 +0200

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        /*
-
-
-
-long x = 10;
-int y = (int)x;
-
-
-*/
-
-        int a = 5;
-        double b = 2;
         double c = Double.MAX_VALUE /0.3D;
         System.out.println(Integer.MAX_VALUE);
         System.out.println(c);
-
-        long x = 10;
-        int y = (int) x;
 
         String text1 = "text";
         String text2 = new String("text");
         System.out.println(text1.equals(text2));
 
-
-        Integer integer1 = 129;
-        Integer integer2 = 129;
         Boolean b1 = false;
         Boolean b2 = false;
         System.out.println(b2 == b1);
